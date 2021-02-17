f934ac4edc99c18a0c3a1677048e8d63875bde2e
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2021-02-17 17:47:32 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,4 +2,18 @@ from app import *
 
 import pytest
 
-def test_c2f():
+def test_createFile():
+  assert createFile() == "File Created!"
+   
+def test_deleteFile():
+  assert deleteFile(12345) == "File Deleted!"
+  assert deleteFile("FILENAME") == "fileID needs to be an integer"
+    
+def test_sentimentNLP():
+  assert sentimentNLP(12345) == "Text Analyzed!"
+  assert sentimentNLP("abcede") == "fileID needs to be an integer" 
+
+def test_createFeed():
+  assert createFeed() == "Feed Created!"
+    
+ 
