cd2069f95f088d5d9703a7fac43558fe90e2dd1c
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2021-02-17 17:47:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/upload/')
+@app.route('/uploader/create')
 def createFile():
     return jsonify({'fileID':'TEST',
                     'size':'TEST'})
 
+@app.route('/uploader/delete')
+def deleteFile(fileID):
+    return jsonify({'fileID':'TEST',
+                    'size':'TEST'})
+
 @app.route('/textnlp/', methods=['GET', 'POST'])
-def sentimentnlp():
-    return "Text NLP Stub!"
+def sentimentnlp(inString):
+    return "Text Analyzed!"
 
 @app.route('/newsfeed/', methods=['GET', 'POST'])
 def createFeed():
-    return "Newsfeed Stub!"
+    return "Feed Created!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
