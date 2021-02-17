4f104a04485cbf63fdadd7f63a8546c27967b401
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2021-02-17 17:57:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
 
 @app.route('/uploader/create')
 def createFile():
-    return jsonify({'fileID':'TEST',
-                    'size':'TEST'})
+    logging.info("File Creation Started...")
+    logging.info("File Created!")
+#    return jsonify({'fileID':'TEST',
+#                     'size':'TEST'})
+    return "File Created!"
+
 
 @app.route('/uploader/delete')
 def deleteFile(fileID):
-    return jsonify({'fileID':'TEST',
-                    'size':'TEST'})
+    logging.info("File Started...")
+    logging.info("File Created!")
+#     return jsonify({'fileID':'TEST',
+#                     'size':'TEST'})
+    return "File Deleted!"
+
 
 @app.route('/textnlp/', methods=['GET', 'POST'])
 def sentimentnlp(inString):
+    logging.info("Text Analyzing...")
+    logging.info("Text Analyzed!")
+    
     return "Text Analyzed!"
 
 @app.route('/newsfeed/', methods=['GET', 'POST'])
 def createFeed():
+    logging.info("Newsfeed Generating...")
+    logging.info("Feed Created!")
     return "Feed Created!"
 
 if __name__ == '__main__':
